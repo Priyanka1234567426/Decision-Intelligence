4ad25eb2ac9cff9d47a6527d884c1e8c3df72327
Priyanka1234567426 <priyankagudipudi@gmail.com>
2026-03-04 18:49:51 +0530

    The correct PORT

--- backend/server.js
+++ backend/server.js
@@ -103,7 +103,7 @@ app.use((req, res) => {
 START SERVER
 --------------------------------
 */
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
