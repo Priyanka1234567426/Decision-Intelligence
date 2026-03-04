2cde0461e9078baab05a2f6f554d8579dbf28cc8
Priyanka1234567426 <priyankagudipudi@gmail.com>
2026-03-04 18:32:25 +0530

    Changed the CORS origin

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json({ limit: "10mb" }));
 
 app.use(cors({
