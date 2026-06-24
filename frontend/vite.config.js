5f2522bc53dbd68a6dc813f527048761eca4ff11
Priyanka1234567426 <priyankagudipudi@gmail.com>
2026-06-24 16:08:38 +0530

    initate React app v1.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        landing: "./index-landing.html",
-      },
-    },
     outDir: "dist",
     sourcemap: false,
   },
